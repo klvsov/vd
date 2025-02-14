300b5d4f423fdfb24957e7da37eed1d58e044d31
klvsov <klvsov@gmail.com>
2025-02-14 13:16:45 +0200

    added opacity transition for text

--- script.js
+++ script.js
@@ -2,24 +2,35 @@ document.addEventListener("DOMContentLoaded", () => {
     const container = document.querySelector(".container");
     const card = document.querySelector(".card");
     const body = document.querySelector("body");
+    const text = document.querySelector(".text");
+
+    console.log({text})
 
     container.addEventListener("mouseenter", () => {
         card.style.transition = "top 0.5s";
         card.style.top = "-90px";
+        text.style.transition = "opacity 0.5s";
+        text.style.opacity = 1
     });
 
     container.addEventListener("mouseleave", () => {
         card.style.transition = "top 0.5s";
         card.style.top = "0";
+        text.style.transition = "opacity 0.5s";
+        text.style.opacity = 0
     });
 
     container.addEventListener("click", () => {
         card.style.transition = "top 0.5s";
         card.style.top = "-90px";
+        text.style.transition = "opacity 0.5s";
+        text.style.opacity = 1
     }, );
 
     body.addEventListener("click", () => {
         card.style.transition = "top 0.5s";
         card.style.top = "0";
+        text.style.transition = "opacity 0.5s";
+        text.style.opacity = 0
     }, true);
 });
